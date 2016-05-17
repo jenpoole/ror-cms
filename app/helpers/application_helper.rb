95db8352dfc4706bfad6e3f430b3e677844601b1
Jen <contact.vandepool@gmail.com>
2016-05-17 05:13:51 +0000

    publicly route pages using page slug or page id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    # display correct page url
+    # if page slug exists, display slug
+    # otherwise, display page id
+    def url_for_page page
+        if page.slug.present?
+            page.slug
+        else
+            "pages/#{page.id}"
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  # show-only public path for pages without slugs
+  resources :pages, only: [:show]
 
+  # create paths /admin/pages
   namespace :admin do
-    resources :pages # /admin/pages
+    resources :pages 
+  end
+  
+  # loop through each page (that has a slug) and create route based on their slug
+  Page.where.not("slug", nil).all.each do |page|
+    get "/#{page.slug}", controller: "pages", action: "show", id: page.id
   end
   
   # You can have the root of your site routed with "root"
